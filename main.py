d320fdb05d9d0120b607ca446fcce4349064c1b1
alpcan <78053276+Alpcannn@users.noreply.github.com>
2021-04-03 13:24:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+exam_note= int(input("enter your exam note: "))
+
+if exam_note >= 90:
+    print("exam note AA")
+elif exam_note >= 80:
+    print("exam note BA")
+elif exam_note >= 70:
+    print("exam note BB")
+elif exam_note >= 60:
+    print("exam note CB")
+else :
+    print("you failed the exam")
